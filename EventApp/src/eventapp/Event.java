db56c935df4caa19cfc225ecc132a44d0c6cbba4
Christopher <christopher.forget@hotmail.ca>
2023-01-16 21:00:17 -0500

    first commit

---
+++ EventApp/src/eventapp/Event.java
@@ -0,0 +1,39 @@
+package eventapp;
+
+/**
+ * @author Christopher Forget
+ */
+
+import java.util.Date;
+
+public class Event {
+    
+    private String title;
+    private String pubDate;
+    private String link;
+    private String guid;
+    private String description;
+    
+    public Event(String title, String link, String guid, String pubDate, String description){
+        this.title = title;
+        this.link = link;
+        this.guid = guid;
+        this.pubDate = pubDate;
+        this.description = description;
+    }
+    
+    
+    public String getTitle() {return title;}
+    public String getPubDate() {return pubDate;}
+    public String getLink() {return link;}
+    public String getGuid() {return guid;}
+    public String getDescription() {return description;}
+    
+    public void setTitle(String title) {this.title = title;}
+    public void setPubDate(String pubDate) {this.pubDate = pubDate;}
+    public void setLink(String link) {this.link = link;}
+    public void setGuid(String guid) {this.guid = guid;}
+    public void setDescription(String description) {this.description = description;}
+    
+    
+}
---
+++ EventApp/src/eventapp/XMLParser.java
@@ -0,0 +1,49 @@
+package eventapp;
+
+/**
+ * @author Christopher Forget
+ */
+
+import java.util.ArrayList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class XMLParser {
+    
+    private Document document;
+        
+    public XMLParser(){}
+    
+    public Document parse(String url) throws Exception{
+        // Loads an XML file and builds a DOM tree document.
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(false);
+        DocumentBuilder parser = factory.newDocumentBuilder();
+        document = parser.parse(url);
+        return document;
+    }
+    
+    public Element getElement(String elementName){
+        //Access channel
+        NodeList nl = document.getElementsByTagName(elementName);
+        Element element = (Element) nl.item(0);
+        
+        return element;
+    }
+    
+    public ArrayList<Element> getElementList(String elementsTag){
+        ArrayList<Element> items = new ArrayList<>();
+        NodeList nl = document.getElementsByTagName(elementsTag);
+
+        //Pour chaque Node item of va chercher le titre, la date, le lien
+        for (int i = 0; i < nl.getLength(); i++){
+            Element element = (Element) nl.item(i);
+            items.add(element);
+        } 
+        return items;
+    }   
+}
